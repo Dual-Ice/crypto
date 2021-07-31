66c94a54b6508bb7b9c78aad0f9da8bb86a3fe74
Maxim Stogniy <i.dual.ce@gmail.com>
2021-07-31 23:04:58 +1200

    Preparing for WebSockets

--- src/api.js
+++ src/api.js
@@ -1,19 +1,51 @@
-const api_key = 'f0f1310977798a0f65fdfd650d57f6e36c453a03d90e358ea1834d873a451c38';
+const API_KEY = 'f0f1310977798a0f65fdfd650d57f6e36c453a03d90e358ea1834d873a451c38';
+const tickersHandlers = new Map();
 
+const loadTickers = () => {
+	if (tickersHandlers.size === 0) {
+		return
+	}
 
-export const loadTickers = tickers => 
 	fetch(
-	  `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${tickers}&tsyms=USD&api_key=${api_key}`
+	  `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${[...tickersHandlers.keys()].join(',')}&tsyms=USD&api_key=${API_KEY}`
 	)
 	.then(r =>r.json())
-  .then(rawData => 
-  	Object.fromEntries(
+  .then(rawData => {
+  	const updatedPrices =	Object.fromEntries(
   		Object.entries(rawData)
 	  		.map(([key, value]) => [key, value.USD]))
-	);
+  	Object.entries(updatedPrices).forEach(([currency, newPrice]) => {
+  		const handlers = tickersHandlers.get(currency) ?? [];
+  		handlers.forEach(handler => handler(newPrice));
+  	})
+  });
+}
+
+// REST API	
+// export const loadTickers = tickers => 
+// 	fetch(
+// 	  `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${tickers}&tsyms=USD&api_key=${API_KEY}`
+// 	)
+// 	.then(r =>r.json())
+//   .then(rawData => 
+//   	Object.fromEntries(
+//   		Object.entries(rawData)
+// 	  		.map(([key, value]) => [key, value.USD]))
+// 	);
 
 export const loadCoins = () =>
 	fetch(
       "https://min-api.cryptocompare.com/data/all/coinlist?summary=true"
   )
   .then(r => r.json());
+
+export const subscribeToTicker = (ticker, cb) => {
+	const subscribers = tickersHandlers.get(ticker) || [];
+	tickersHandlers.set(ticker, [...subscribers, cb]);
+} 
+
+export const unSubscribeFromTicker = (ticker) => {
+	tickersHandlers.delete(ticker);
+} 
+
+setInterval(loadTickers, 3000);
